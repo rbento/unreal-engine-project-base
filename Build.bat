5c0e86119325b03ed715e2ec4a97564029a22c47
Rodrigo Bento <rodrigo.bento@gmail.com>
2024-07-09 12:09:16 -0400

    Add Build.bat script

---
+++ Build.bat
@@ -0,0 +1,13 @@
+@echo off
+
+set ROOTDIR=%~dp0
+set ROOTDIR=%ROOTDIR:~0,-1%
+
+set PROJECT=%1
+set PROJECT_DIR=%ROOTDIR%\%PROJECT%
+set UPROJECT_PATH=%PROJECT_DIR%\%PROJECT%.uproject
+
+set UE5_DIR=C:\Program Files\Epic Games\UE_5.4
+set BUILD_BAT=%UE5_DIR%\Engine\Build\BatchFiles\Build.bat
+
+call "%BUILD_BAT%" %PROJECT%Editor Win64 Development "%UPROJECT_PATH%" -waitmutex -NoHotReload
