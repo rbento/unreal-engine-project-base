645d0a92bd2af27b1ee04851f5114263d2f88a71
Rodrigo Bento <rodrigo.bento@gmail.com>
2024-08-27 09:01:42 -0400

    Add option to build standalone project with automatic cooking

--- Scripts/Build.bat
+++ Scripts/Build.bat
@@ -1,38 +1,23 @@
 @ECHO OFF
 
-IF "%UE_VERSION%" == "" (
-    ECHO ABORTED: Environment Variable 'UE_VERSION' is not defined
-    EXIT /B
-)
+SETLOCAL
 
-IF "%UE_HOME%" == "" (
-    ECHO ABORTED: Environment Variable 'UE_HOME' is not defined
-    EXIT /B
-)
+CALL Vars.bat %1 || EXIT /B 
 
-IF "%1" == "" (
-    ECHO ABORTED: Unreal project name name was not provided
-    ECHO EXAMPLE: Build MyProject
-    EXIT /B
+SET MODE=Editor
+IF "%2" == "-standalone" (
+    SET MODE=
+    CALL Cook.bat %1 || EXIT /B 
 )
-
-SET UE=%UE_HOME%%UE_VERSION%
 
 ECHO Building with Unreal Engine at %UE%
-
-SET ROOT_DIR=%~dp0
-SET ROOT_DIR=%ROOT_DIR:~0,-1%
-
-SET PROJECT=%1
-SET PROJECT_DIR=%ROOT_DIR%\%PROJECT%
-SET UPROJECT_PATH=%PROJECT_DIR%\%PROJECT%.uproject
 
 ECHO Project: %UPROJECT_PATH%
 
 SET BUILD_BAT=%UE%\Engine\Build\BatchFiles\Build.bat
 
-SET BUILD_CMD="%BUILD_BAT%" %PROJECT%Editor Win64 Development "%UPROJECT_PATH%" -waitmutex -NoHotReload
+SET BUILD_CMD="%BUILD_BAT%" %PROJECT%%MODE% Win64 Development "%UPROJECT_PATH%" -waitmutex -NoHotReload
 
-ECHO Build Command: %BUILD_CMD%
+ECHO Command: %BUILD_CMD%
 
 CALL %BUILD_CMD%
