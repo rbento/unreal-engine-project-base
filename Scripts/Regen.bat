b9cbf4a7aa84d3c7b587b84602119c8b4cf207da
Rodrigo Bento <rodrigo.bento@gmail.com>
2024-09-20 12:25:50 -0400

    Update Regen.bat

--- Scripts/Regen.bat
+++ Scripts/Regen.bat
@@ -4,20 +4,16 @@ SETLOCAL
 
 SET CURRENT_DIR=%~dp0
 
-SET UDATE=%DATE%
-
-FOR /f "tokens=*" %%A IN ("%TIME%") DO SET UTIME=%%~pnxA
-SET UTIME=%UTIME:~1%
-
 CALL "%CURRENT_DIR%Vars.bat" %1 || EXIT /B 
 
 ECHO Running with Unreal Engine at %UE%
 
 ECHO Project: %UPROJECT_PATH%
 
-SET REGEN_CMD="%UE_BUILD_TOOL_BAT%" -projectfiles -project="%UPROJECT_PATH%" -game -rocket -progress -log="%UPROJECT_DIR%/Saved/Logs/UnrealVersionSelector-%UDATE%-%UTIME%.log"
+SET ULOG_SUFFIX=%DATE%-%RANDOM%%RANDOM%%RANDOM%
+
+SET REGEN_CMD="%UE_BUILD_TOOL_BAT%" -projectfiles -project="%UPROJECT_PATH%" -game -rocket -progress -log="%UPROJECT_DIR%/Saved/Logs/UnrealVersionSelector-%ULOG_SUFFIX%.log"
 
 ECHO Command: %REGEN_CMD%
 
 CALL %REGEN_CMD% || EXIT /B 
-
